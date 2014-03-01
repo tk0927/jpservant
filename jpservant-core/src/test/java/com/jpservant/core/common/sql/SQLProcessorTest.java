5d0b346316501a9d0f5d5c43df04111f02f8f589
tk0927 <toshiaki.kamoshida@gmail.com>
2014-03-01 19:57:25 +0900

    テストコード Assertion追記

--- jpservant-core/src/test/java/com/jpservant/core/common/sql/SQLProcessorTest.java
+++ jpservant-core/src/test/java/com/jpservant/core/common/sql/SQLProcessorTest.java
@@ -1,6 +1,7 @@
 package com.jpservant.core.common.sql;
 
 import static com.jpservant.test.miscellaneous.TestUtilities.*;
+import static junit.framework.Assert.*;
 
 import org.junit.Test;
 
@@ -33,32 +34,44 @@ public class SQLProcessorTest {
 
 		DataCollection rows = new DataCollection();
 		DataObject row1 = new DataObject();
-		row1.put("StringCol", "AAA");
-		row1.put("TimestampCol", "2014-01-01 00:00:01");
-		row1.put("NumberCol", 1);
-		row1.put("BoolCol", 0);
+		row1.put("STRING_COL", "AAA");
+		row1.put("TIMESTAMP_COL", "2014-01-01 00:00:01.0");
+		row1.put("NUMBER_COL", 1);
+		row1.put("BOOL_COL", 0);
 		rows.add(row1);
 
 		DataObject row2 = new DataObject();
-		row2.put("StringCol", "BBB");
-		row2.put("TimestampCol", null);
-		row2.put("NumberCol", null);
-		row2.put("BoolCol", null);
+		row2.put("STRING_COL", "BBB");
+		row2.put("TIMESTAMP_COL", null);
+		row2.put("NUMBER_COL", null);
+		row2.put("BOOL_COL", null);
 		rows.add(row2);
 
 		processor.executeUpdate(
 				"INSERT INTO T_SAMPLE_TEST_TABLE(STRING_COL,TIMESTAMP_COL,NUMBER_COL,BOOL_COL)" +
-				" VALUES(:StringCol,:TimestampCol,:NumberCol,:BoolCol)",rows);
+				" VALUES(:STRING_COL,:TIMESTAMP_COL,:NUMBER_COL,:BOOL_COL)",rows);
 
-		DataCollection result = processor.executeQuery(
-				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :StringCol", row1);
-		System.out.println("result1=" + result);
+		DataCollection result1 = processor.executeQuery(
+				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :STRING_COL", row1);
+		assertEquals(result1.size(),1);
+		DataObject resultrow1 = result1.get(0);
+		assertEquals(resultrow1.get("STRING_COL"),"AAA");
+		assertEquals(resultrow1.get("TIMESTAMP_COL"),"2014-01-01 00:00:01.0");
+		assertEquals(resultrow1.get("NUMBER_COL"),"1");
+		assertEquals(resultrow1.get("BOOL_COL"),"0");
+
 
 		DataCollection result2 = processor.executeQuery(
-				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :StringCol", row2);
-		System.out.println("result2=" + result2);
+				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :STRING_COL", row2);
+		assertEquals(result2.size(),1);
+		DataObject resultrow2 = result2.get(0);
+		assertEquals(resultrow2.get("STRING_COL"),"BBB");
+		assertNull(resultrow2.get("TIMESTAMP_COL"));
+		assertNull(resultrow2.get("NUMBER_COL"));
+		assertNull(resultrow2.get("BOOL_COL"));
 
-		CONNECTION_HOLDER.commit();
+		CONNECTION_HOLDER.rollback();
+
 	}
 
 }
