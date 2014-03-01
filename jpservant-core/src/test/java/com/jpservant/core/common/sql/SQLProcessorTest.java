29e196ae4c81e6dfef3ef62b356c1c376d1a043f
tk0927 <toshiaki.kamoshida@gmail.com>
2014-03-01 20:09:34 +0900

    テストケース修正

--- jpservant-core/src/test/java/com/jpservant/core/common/sql/SQLProcessorTest.java
+++ jpservant-core/src/test/java/com/jpservant/core/common/sql/SQLProcessorTest.java
@@ -30,8 +30,10 @@ public class SQLProcessorTest {
 
 		SQLProcessor processor = new SQLProcessor( CONNECTION_HOLDER );
 
+		//テーブルクリア
 		processor.executeUpdate("DELETE FROM T_SAMPLE_TEST_TABLE");
 
+		//テスト用データの挿入
 		DataCollection rows = new DataCollection();
 		DataObject row1 = new DataObject();
 		row1.put("STRING_COL", "AAA");
@@ -51,8 +53,11 @@ public class SQLProcessorTest {
 				"INSERT INTO T_SAMPLE_TEST_TABLE(STRING_COL,TIMESTAMP_COL,NUMBER_COL,BOOL_COL)" +
 				" VALUES(:STRING_COL,:TIMESTAMP_COL,:NUMBER_COL,:BOOL_COL)",rows);
 
+		//テスト用データの検索結果確認
+		DataObject criteria1 = new DataObject();
+		criteria1.put("STRING_COL", "AAA");
 		DataCollection result1 = processor.executeQuery(
-				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :STRING_COL", row1);
+				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :STRING_COL", criteria1);
 		assertEquals(result1.size(),1);
 		DataObject resultrow1 = result1.get(0);
 		assertEquals(resultrow1.get("STRING_COL"),"AAA");
@@ -60,9 +65,10 @@ public class SQLProcessorTest {
 		assertEquals(resultrow1.get("NUMBER_COL"),"1024");
 		assertEquals(resultrow1.get("BOOL_COL"),"0");
 
-
+		DataObject criteria2 = new DataObject();
+		criteria2.put("STRING_COL", "BBB");
 		DataCollection result2 = processor.executeQuery(
-				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :STRING_COL", row2);
+				"SELECT * FROM T_SAMPLE_TEST_TABLE WHERE STRING_COL = :STRING_COL", criteria2);
 		assertEquals(result2.size(),1);
 		DataObject resultrow2 = result2.get(0);
 		assertEquals(resultrow2.get("STRING_COL"),"BBB");
@@ -70,6 +76,7 @@ public class SQLProcessorTest {
 		assertNull(resultrow2.get("NUMBER_COL"));
 		assertNull(resultrow2.get("BOOL_COL"));
 
+		//トランザクションロールバック(DB状態復元)
 		CONNECTION_HOLDER.rollback();
 
 	}
