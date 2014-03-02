f446c3455ebf9e5efea63dcd3c259bf93b59a9f7
tk0927 <toshiaki.kamoshida@gmail.com>
2014-03-02 15:28:24 +0900

    JDBC Connectionのスコープ別クラスを作成、 テスト用をテスト側ソースディレクトリに移動。

--- jpservant-core/src/test/java/com/jpservant/test/miscellaneous/TestJDBCDriverManagerResource.java
+++ jpservant-core/src/test/java/com/jpservant/test/miscellaneous/TestJDBCDriverManagerResource.java
@@ -1,21 +1,22 @@
-package com.jpservant.core.resource;
+package com.jpservant.test.miscellaneous;
 
 import com.jpservant.core.common.sql.impl.DriverManagerConnectionHolder;
 import com.jpservant.core.module.spi.ModuleConfiguration;
+import com.jpservant.core.resource.ResourcePlatform;
 
 /**
  *
  * JDBC DriverManagerに基づくデータベースコネクションを所有するリソースクラス。
  *
  * <pre>
- * リソースを要求されるたびに、設定情報を利用してDB接続を行う。
+ * JVMの稼働期間中、同一コネクションを継続して利用する。
  * </pre>
  *
  * @author Toshiaki.Kamoshida <toshiaki.kamoshida@gmail.com>
  * @version 0.1
  *
  */
-public class JDBCDriverManagerResource implements ResourcePlatform {
+public class TestJDBCDriverManagerResource implements ResourcePlatform {
 
 	public static enum ConfigurationName{
 		JDBCDriver,
@@ -24,23 +25,23 @@ public class JDBCDriverManagerResource implements ResourcePlatform {
 		JDBCPassword,
 	}
 
-	private ModuleConfiguration config;
+	private DriverManagerConnectionHolder impl;
 
 	@Override
 	public void initialize(ModuleConfiguration config) {
-		this.config = config;
-	}
 
-	@Override
-	public Object getResource() {
-
-		return new DriverManagerConnectionHolder(
+		this.impl = new DriverManagerConnectionHolder(
 				(String)config.get(ConfigurationName.JDBCDriver.name()),
 				(String)config.get(ConfigurationName.JDBCURL.name()),
 				(String)config.get(ConfigurationName.JDBCUser.name()),
 				(String)config.get(ConfigurationName.JDBCPassword.name())
 			);
 
+	}
+
+	@Override
+	public Object getResource() {
+		return this.impl;
 	}
 
 }
