2c7c66eb7714498519bb775e099fba57a69e6caf
tk0927 <toshiaki.kamoshida@gmail.com>
2014-03-08 12:01:42 +0900

    DAOモジュール PrimaryKey指定参照・更新 動作確認OK

---
+++ jpservant-core/src/main/java/com/jpservant/core/module/dao/impl/action/ActionUtils.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2014 Toshiaki Kamoshida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jpservant.core.module.dao.impl.action;
+
+import static com.jpservant.core.common.Utilities.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jpservant.core.common.DataCollection;
+import com.jpservant.core.common.DataObject;
+
+/**
+ *
+ * Action向けのユーティリティメソッドを提供します。
+ *
+ * @author Toshiaki.Kamoshida <kamoshida.toshiaki@gmail.com>
+ * @version 0.1
+ *
+ */
+public class ActionUtils {
+
+	public static String createColumnsToken(List<String> columnnames) {
+
+		StringBuilder sb = new StringBuilder();
+		for (String colname : columnnames) {
+			sb.append(colname);
+			sb.append(",");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+
+	}
+
+	public static String createInsertPlaceholderToken(List<String> columnnames) {
+
+		StringBuilder sb = new StringBuilder();
+		for (String colname : columnnames) {
+			sb.append(":");
+			sb.append(convertSnakeToCamel(colname));
+			sb.append(",");
+		}
+
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+	public static String createUpdatePlaceholderToken(List<String> columnnames) {
+
+		StringBuilder sb = new StringBuilder();
+		for (String colname : columnnames) {
+			sb.append(colname);
+			sb.append("=:");
+			sb.append(convertSnakeToCamel(colname));
+			sb.append(",");
+		}
+
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+	public static String createWhereClause(List<String> colnames) {
+
+		StringBuilder sb = new StringBuilder();
+		for (String colname : colnames) {
+			sb.append(colname);
+			sb.append("=:");
+			sb.append(convertSnakeToCamel(colname));
+			sb.append(" AND ");
+		}
+
+		return sb.substring(0, sb.length() - 4);
+	}
+
+	public static String createWhereClause(List<String> colnames,List<String> bindnames) {
+
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+		for (String colname : colnames) {
+			sb.append(colname);
+			sb.append("=:");
+			sb.append(bindnames.get(i++));
+			sb.append(" AND ");
+		}
+
+		return sb.substring(0, sb.length() - 4);
+	}
+
+	public static String createOrderByClause(List<String> colnames) {
+
+		StringBuilder sb = new StringBuilder();
+		for (String colname : colnames) {
+			sb.append(colname);
+			sb.append(",");
+		}
+
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+	public static DataObject bindPathTokens(List<String> colnames, List<String> values) {
+
+		DataObject retvalue = new DataObject();
+
+		for (int i = 0; i < colnames.size(); i++) {
+			retvalue.put(colnames.get(i), values.get(i));
+		}
+
+		return retvalue;
+
+	}
+
+	public static DataCollection bindPathTokens(DataCollection list, List<String> colnames, List<String> values) {
+
+		DataCollection retvalue = new DataCollection();
+		for (DataObject obj : list) {
+
+			DataObject element = new DataObject(obj);
+			for (int i = 0; i < colnames.size(); i++) {
+				element.put(colnames.get(i), values.get(i));
+			}
+			retvalue.add(element);
+		}
+		return retvalue;
+	}
+
+	public static List<String> appendPrefixes(List<String> list, String prefix) {
+
+		List<String> retvalue = new ArrayList<String>();
+		for (String obj : list) {
+			retvalue.add(prefix + obj);
+		}
+		return retvalue;
+	}
+
+}
---
+++ jpservant-core/src/main/java/com/jpservant/core/module/dao/impl/action/DeleteByPrimaryKeyAction.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2014 Toshiaki Kamoshida
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jpservant.core.module.dao.impl.action;
+
+import static com.jpservant.core.module.dao.impl.action.ActionUtils.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.jpservant.core.common.DataCollection;
+import com.jpservant.core.common.DataObject;
+import com.jpservant.core.common.sql.SQLProcessor;
+import com.jpservant.core.kernel.KernelContext;
+import com.jpservant.core.module.dao.impl.DataAccessAction;
+import com.jpservant.core.module.spi.ModuleConfiguration;
+
+/**
+ *
+ * プライマリーキー指定での削除Action。
+ *
+ * @author Toshiaki.Kamoshida <toshiaki.kamoshida@gmail.com>
+ * @version 0.1
+ *
+ */
+public class DeleteByPrimaryKeyAction implements DataAccessAction {
+
+	private String sql;
+	private List<String> primarykeynames;
+
+	public DeleteByPrimaryKeyAction(String tablename, List<String> primarykeynames) {
+
+		this.primarykeynames = primarykeynames;
+		this.sql = String.format("DELETE FROM %s WHERE %s",
+				tablename,
+				createWhereClause(primarykeynames));
+
+	}
+
+	@Override
+	public DataCollection execute(
+			SQLProcessor processor, ModuleConfiguration config, KernelContext context, List<String> pathtokens)
+			throws SQLException {
+
+		int result = processor.executeUpdate(this.sql,
+				bindPathTokens(this.primarykeynames, pathtokens));
+		return new DataCollection(new DataObject("Count", result));
+
+	}
+}
